7618c2b6c9a0326952e7fc72aa494c20c21788f0
Naman Singh <snaman431@gmail.com>
2021-11-09 20:04:15 +0530

    router and model setup

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
+
+app.use(express.json({extended:false}));
 
 const PORT = process.env.PORT || 5000;
 
