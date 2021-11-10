b300d63a4ef2b145ae10d482f2298409caa20e4b
Naman Singh <snaman431@gmail.com>
2021-11-10 20:36:47 +0530

    backend done

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,20 @@ const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('Get logged in user');
+router.get('/',auth,async (req,res)=>{
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json({
+            user
+        });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 router.post('/',[
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,13 +1,53 @@
 const express = require('express');
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
+const Contact = require('../models/Contact');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('Get contacts of the user');
+router.get('/',auth,async (req,res)=>{
+    try{
+        const contacts = await Contact.find({user:req.user.id}).sort({date:-1});
+        res.json({
+            contacts
+        });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
-router.post('/',(req,res)=>{
-    res.send('Add new contact');
+router.post('/',[auth,[
+    check('name','Name is required').not().isEmpty()
+]],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({
+            errors : errors.array()
+        });
+    }
+    const {name,email,phone,type} = req.body;
+    try{
+
+        const contact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user:req.user.id
+        });
+
+        await contact.save();
+
+        res.json({
+            contact
+        });
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
 });
 
 router.put('/:id',(req,res)=>{
