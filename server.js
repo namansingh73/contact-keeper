8edff371ff697a15797f8edc975384209829e998
Naman Singh <snaman431@gmail.com>
2021-11-16 12:10:03 +0530

    minor bug fixes

--- server.js
+++ server.js
@@ -18,8 +18,11 @@ app.use('/api/contacts',require('./routes/contacts'));
 if(process.env.NODE_ENV === 'production')
 {
     app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
+    app.get('/',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+    app.get('*',(req,res)=>{
+        res.redirect('/');
     });
 }
 
